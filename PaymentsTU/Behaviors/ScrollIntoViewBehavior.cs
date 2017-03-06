4dba32e7d0cfdd48775bc610c3f9ae9a4104674c
Henadz <henadz.linevich@gmail.com>
2017-03-07 00:49:59 +0300

    Fix ScrollIntoViewBehavior

--- PaymentsTU/Behaviors/ScrollIntoViewBehavior.cs
+++ PaymentsTU/Behaviors/ScrollIntoViewBehavior.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
+using FrameworkExtend;
 
 namespace PaymentsTU.Behaviors
 {
@@ -50,7 +54,39 @@ namespace PaymentsTU.Behaviors
 		private static void OnSelectedItemChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
 		{
 			var listbox = d as ListBox;
-			listbox?.ScrollIntoView(e.NewValue);
+			if (listbox == null) return;
+
+			if (listbox.IsGrouping)
+			{
+				if (listbox.ItemContainerGenerator.Status == GeneratorStatus.ContainersGenerated)
+				{
+					listbox.Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() => DelayedBringIntoView(listbox, e.NewValue)));
+				}
+				else
+				{
+					listbox.ItemContainerGenerator.StatusChanged += ItemContainerGenerator_StatusChanged;
+				}
+			}
+			else
+			{
+				listbox.ScrollIntoView(e.NewValue);
+			}
+		}
+
+		private static void ItemContainerGenerator_StatusChanged(object sender, EventArgs e)
+		{
+			var control = (ListBox) sender;
+			if (control.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated)
+				return;
+
+			control.ItemContainerGenerator.StatusChanged -= ItemContainerGenerator_StatusChanged;
+			control.Dispatcher.BeginInvoke(DispatcherPriority.Render, new Action(() => DelayedBringIntoView(control, control.SelectedItem)));
+		}
+
+		private static void DelayedBringIntoView(ListBox control, object selectedItem)
+		{
+			var item = control.ItemContainerGenerator.ContainerFromItem(selectedItem) as ListBoxItem;
+			item?.BringIntoView();
 		}
 	}
 }
